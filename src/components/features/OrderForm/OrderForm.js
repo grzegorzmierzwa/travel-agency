b45bdb0c862a5c0434665c4556973580ebae56d8
Grzegorz Mierzwa <mierzwagrzegorz@gmail.com>
2020-03-09 17:48:27 +0100

    Add price function to trips

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -5,16 +5,17 @@ import OrderSummary from '../OrderSummary/OrderSummary';
 
 import {Row, Col} from 'react-flexbox-grid';
 
-const OrderForm = (tripCost) => (
+const OrderForm = ({tripCost, options}) => (
   <Row>
     <Col xs={12}>
-      <OrderSummary tripCost={tripCost} />
+      <OrderSummary tripCost={tripCost} options={options} />
     </Col>
   </Row>
 );
 
 OrderForm.propTypes = {
-  tripCost: PropTypes.string, 
+  tripCost: PropTypes.string,
+  options: PropTypes.object, 
 };
 
 export default OrderForm;
--- src/components/features/OrderSummary/OrderSummary.js
+++ src/components/features/OrderSummary/OrderSummary.js
@@ -2,28 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import {formatPrice} from '../../../utils/formatPrice';
-// import {calculateTotal} from '../../../utils/calculateTotal';
+import {calculateTotal} from '../../../utils/calculateTotal';
 
 import styles from './OrderSummary.scss';
 // import {Row, Col} from 'react-flexbox-grid';
 
-const OrderSummary = (tripCost) => {
-  const priceFormat = formatPrice(tripCost);
-  //   const price = calculateTotal();
-  console.log('cost', priceFormat);
-  console.log('tripCost', tripCost);
-
+const OrderSummary = ({tripCost, options}) => {
+  const price = calculateTotal(formatPrice(tripCost), options);
 
   return (
     <h2 className={styles.component} >
       Total:
-      {/* <strong>{price}</strong> */}
+      <strong> $ {price}</strong>
     </h2>
   );
 };
 
 OrderSummary.propTypes = {
-  tripCost: PropTypes.string, 
+  tripCost: PropTypes.string,
+  options: PropTypes.object,  
 };
 
 export default OrderSummary;
