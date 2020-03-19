2ac7068c2ec86a19a3a8afeffc5a83e5ea5e487f
Grzegorz Mierzwa <mierzwagrzegorz@gmail.com>
2020-03-19 22:59:14 +0100

    Add API

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -8,8 +8,47 @@ import pricing from '../../../data/pricing.json';
 
 import {Row, Col} from 'react-flexbox-grid';
 
+import settings from '../../../data/settings.js';
 
-const OrderForm = ({tripCost, options, setOrderOption}) => (
+import {formatPrice} from '../../../utils/formatPrice';
+import {calculateTotal} from '../../../utils/calculateTotal';
+
+import Button from '../../common/Button/Button';
+
+
+const sendOrder = (options, tripCost, name, countryCode, id) => {
+  const totalCost = formatPrice(calculateTotal(tripCost, options));
+  
+  console.log('name', name);
+
+  const payload = {
+    ...options,
+    totalCost,
+    name,
+    countryCode,
+    id,
+  };
+
+  const url = settings.db.url + '/' + settings.db.endpoint.orders;
+
+  const fetchOptions = {
+    cache: 'no-cache',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  };
+
+  fetch(url, fetchOptions)
+    .then(function(response){
+      return response.json();
+    }).then(function(parsedResponse){
+      console.log('parsedResponse', parsedResponse);
+    });
+};
+
+const OrderForm = ({tripCost, options, setOrderOption, name, countryCode, id}) => (
   <Row>
     {pricing.map(option => (
       <Col key={option.id} md={4}>
@@ -19,6 +58,7 @@ const OrderForm = ({tripCost, options, setOrderOption}) => (
     <Col xs={12}>
       <OrderSummary tripCost={tripCost} options={options} />
     </Col>
+    <Button onClick={() => sendOrder(options, tripCost, name, countryCode, id)}>Order now!</Button>
   </Row>
 );
 
@@ -26,6 +66,9 @@ OrderForm.propTypes = {
   tripCost: PropTypes.string,
   options: PropTypes.object,
   setOrderOption: PropTypes.func,
+  name: PropTypes.string,
+  id: PropTypes.string,
+  countryCode: PropTypes.string,
 };
 
 export default OrderForm;
